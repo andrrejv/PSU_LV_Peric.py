b2512afb945b38f97b9b3d5e46d61859c2be85ff
andrrejv <andrrejv.4@gmail.com>
2026-03-18 16:28:06 +0100

    Add example visualization using matplotlib

---
+++ PSU_LV2_Zad1.py
@@ -0,0 +1,12 @@
+#primjer vizualizacije
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [1,3,3,2,1]
+y= [1,1,2,2,1]
+plt.plot(x, y, 'b', linewidth=1, marker=".",markersize = 9)
+plt.axis([0,4,0,4])
+plt.xlabel('x-os')
+plt.ylabel('y-os')
+plt.title('Primjer')
+plt.show(
